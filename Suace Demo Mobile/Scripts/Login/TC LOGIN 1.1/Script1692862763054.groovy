9fa1ddb11f97bfea88bae2dcad7827b96e4c1508
irfanafandi989 <156739572+irfanafandi989@users.noreply.github.com>
2024-01-16 11:15:10 +0700

    Add files via upload

---
+++ Suace Demo Mobile/Scripts/Login/TC LOGIN 1.1/Script1692862763054.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\IRFAN\\Downloads\\mda-2.0.0-21.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.ImageView'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView -  15.99'), 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText'), 'bob@example.com', 0)
+
+Mobile.setText(findTestObject('android.widget.TextView - Enter Password'), '10203040', 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Login'), 0)
+
+Mobile.verifyElementExist(findTestObject('Login/Massage Error'), 0)
+
+Mobile.closeApplication()
+
